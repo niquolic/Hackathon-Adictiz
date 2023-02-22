f124116af8ee1bd7be12c9a5e995c8b93588db09
niquolic <nicolas.debruycker22@gmail.com>
2023-02-22 13:27:36 +0100

    bornes page

--- hackathon-adictiz/src/App.js
+++ hackathon-adictiz/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navigation from "./components/Navigation";
+import Bornes from "./components/Bornes";
 import "./asset/styles/layouts/index.scss";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <div className="App">
             <Navigation/>
             <img className="background_wave" src="./img/background_wave.png" alt="background_wave"/>
+            <Bornes/>
         </div>
     );
 }
---
+++ hackathon-adictiz/src/components/Bornes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Bornes = () => {
+    return(
+        <div>
+            <h2>Nos autres bornes</h2>
+            <div className="bones-list">
+
+            </div>
+        </div>
+    );
+};
+
+export default Bornes
