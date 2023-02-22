327a7af9dc657607bce578685eb60b52282198d0
NolanMlb <nolanmlb@outlook.fr>
2023-02-22 13:24:30 +0100

    navbar, fonts and wip body

---
+++ hackathon-adictiz/src/components/Concept.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Concept = () => {
+    return(
+        <div className="container-concept">
+            <h1>Notre concept</h1>
+        </div>
+    );
+};
+
+export default Concept;
