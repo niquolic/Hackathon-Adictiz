178d525a5ca0aca01891972480319ccccbfed1d8
niquolic <nicolas.debruycker22@gmail.com>
2023-02-23 13:49:33 +0100

    tree.js

--- hackathon-adictiz/src/App.js
+++ hackathon-adictiz/src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import Bornes from "./components/Bornes";
 import Concept from "./components/Concept";
 import VideoTerminal from "./components/VideoTerminal";
+import Anim from "./components/Anim";
 import Footer from "./components/Footer";
 import "./asset/styles/layouts/index.scss";
 
@@ -13,6 +14,7 @@ function App() {
             <img className="background_wave" src="./img/background_wave.png" alt="background_wave"/>
             <Concept/>
             <VideoTerminal/>
+            <Anim/>
             <Bornes/>
             <Footer/>
         </div>
---
+++ hackathon-adictiz/src/components/Anim.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "../jsx/App";
+
+const Anim = () => {
+    return(
+        <div>
+            <App/>
+        </div>
+    );
+};
+
+export default Anim
